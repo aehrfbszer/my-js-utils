479cdcf443de6e2c63fc20f8ca67bd4fac1c11bb
aehrfbszer <142887235+aehrfbszer@users.noreply.github.com>
2024-02-05 17:17:32 +0800

    make naive ui great again

---
+++ useReactiveNaive.ts
@@ -0,0 +1,39 @@
+import { ComputedRef, Ref } from 'vue'
+
+export const useReactiveNaive = <T extends Record<string, unknown>>(obj: T) => {
+  const rawObj = {
+    ...obj
+  }
+  const innerValue = ref(obj)
+
+  const naiveObj: {
+    [key in keyof T]: Ref<(typeof obj)[key] | null>
+  } = {}
+
+  Object.keys(obj).forEach((key: keyof T) => {
+    naiveObj[key] = computed({
+      get: () => innerValue.value[key] || null,
+      set: (val) => {
+        innerValue.value[key] = val
+      }
+    })
+  })
+
+  const getterValue: {
+    [key in keyof T]: ComputedRef<(typeof obj)[key] | undefined>
+  } = {}
+
+  const reactiveValue = reactive(naiveObj)
+
+  Object.keys(obj).forEach((key: keyof T) => {
+    getterValue[key] = computed(() => reactiveValue[key] ?? undefined)
+  })
+
+  const resetValue = () => {
+    Object.keys(obj).forEach((key: keyof T) => {
+      reactiveValue[key] = rawObj[key]
+    })
+  }
+
+  return { reactiveValue, jsonObj: getterValue, resetValue }
+}
