757e97762f364c3160e8c349ccf0d925a33d351b
mizuki <mizukishuiyue8@gmail.com>
2024-08-30 17:45:29 +0800

    🦄 refactor: vue的ts是不是有点怪，还是我不会

--- useReactiveNaive.ts
+++ useReactiveNaive.ts
@@ -1,16 +1,21 @@
 import { ComputedRef, Ref, ref, computed, reactive } from 'vue'
 
+// make naive ui great again(但是说实话，没啥用)
 export const useReactiveNaive = <T extends Record<string, unknown>>(obj: T) => {
   const rawObj = {
     ...obj
   }
   const innerValue = ref(obj)
 
-  const naiveObj: {
-    [key in keyof T]: Ref<(typeof obj)[key] | null>
-  } = {}
+  type ObjForNaive = {
+    [key in keyof T]: Ref<T[key] | null>
+  }
 
-  Object.keys(obj).forEach((key: keyof T) => {
+
+  // naive ui不识别空字符，只识别null，赋值为null才会更新页面，置空，所以写naiveObj
+  const naiveObj: ObjForNaive = {} as ObjForNaive
+
+  (Object.keys(obj) as (keyof T)[]).forEach((key) => {
     naiveObj[key] = computed({
       get: () => (typeof innerValue.value[key] === 'number' ? innerValue.value[key] : innerValue.value[key] || null),
       set: (val) => {
@@ -19,18 +24,22 @@ export const useReactiveNaive = <T extends Record<string, unknown>>(obj: T) => {
     })
   })
 
-  const getterValue: {
+  type GetterValueType = {
     [key in keyof T]: ComputedRef<(typeof obj)[key] | undefined>
-  } = {}
+  }
 
-  const reactiveValue = reactive(naiveObj)
+  const getterValue: GetterValueType = {} as GetterValueType
 
-  Object.keys(obj).forEach((key: keyof T) => {
-    getterValue[key] = computed(() => reactiveValue[key] ?? undefined)
+  const reactiveValue = reactive(naiveObj);
+
+  (Object.keys(obj) as (keyof T)[]).forEach((key) => {
+    getterValue[key] = computed(() => naiveObj[key].value ?? undefined)
   })
 
+  // 过滤空字符，null，这两种会转为undefined，在接口中会自然过滤掉undefined字段
   const jsonObj = reactive(getterValue)
 
+  // 忘了这个干嘛的
   const resetValue = () => {
     Object.keys(obj).forEach((key: keyof T) => {
       reactiveValue[key] = rawObj[key]
